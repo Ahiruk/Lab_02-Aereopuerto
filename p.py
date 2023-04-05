c390833f871b44d86a291b7e248aae9631876db3
ahiruk <luisaguzmanidetp@gmail.com>
2023-04-05 14:15:32 -0500

    Mapa y marcadores

--- p.py
+++ p.py
@@ -3,27 +3,37 @@ import dash
 from dash import html
 from dash import dcc
 from dash.dependencies import Input, Output, State
-
+import pandas as pd
 import webbrowser
 
 
-# Crear la aplicación Dash
-app = dash.Dash(__name__)
+#se lee el archivo csv
+capitales = pd.read_csv("europa.csv")
 
 # Crear el mapa de Folium
-m = folium.Map(location=[54.5260, 15.2551], zoom_start=4)
+m = folium.Map(location=[52, 20], zoom_start=4.3)
 
-# Añadir un marcador en Madrid, España
-madrid_marker = folium.Marker(location=[40.4168, -3.7038], tooltip="Madrid, España", customdata="Madrid")
-madrid_marker.add_to(m)
+#creamos un marcador para cada ciudad
+for i, row in capitales.iterrows():
+    folium.Marker(location=[row["Latitud"], row["Longitud"]], 
+                  popup=row["Capital"]).add_to(m)
 
-# Añadir un marcador en París, Francia
-paris_marker = folium.Marker(location=[48.8566, 2.3522], tooltip="París, Francia", customdata="Paris")
-paris_marker.add_to(m)
+# Crear la aplicación Dash
+app = dash.Dash(__name__)
+  
+@app.callback(
+    Output('selected-markers-list', 'children'),
+    [Input('selected-markers', 'data')]
+)
+def update_selected_markers_list(selected_markers):
+    if selected_markers:
+        return [html.Li(marker['popup']) for marker in selected_markers]
+    else:
+        return "No hay marcadores seleccionados"
 
-# Añadir un marcador en Berlín, Alemania
-berlin_marker = folium.Marker(location=[52.5200, 13.4050], tooltip="Berlín, Alemania", customdata="Berlin")
-berlin_marker.add_to(m)
+
+
+
 
 # Convertir el mapa de Folium en un objeto HTML
 html_map = m._repr_html_()
@@ -31,8 +41,7 @@ html_map = m._repr_html_()
 # Definir el diseño de la aplicación Dash
 app.layout = html.Div([
     html.H1("Mapa de Europa"),
-    dcc.Graph(id='map', figure=m._repr_html_()),
-    #html.Iframe(id='map', srcDoc=html_map, width='100%', height='600'),
+    html.Iframe(id='map', srcDoc=html_map, width='100%', height='700'),
     html.Label("Marcadores seleccionados:"),
     dcc.Store(id='selected-markers', data=[]),
     html.Ul(id='selected-markers-list'),
@@ -40,23 +49,7 @@ app.layout = html.Div([
 ])
 
 
-# Crear una función para actualizar la lista de marcadores seleccionados y el textlabel
-@app.callback(
-    [Output('selected-markers', 'data'), Output('selected-markers-label', 'children'), Output('selected-markers-list', 'children')],
-    Input('map', 'clickData'),
-    State('selected-markers', 'data')
-)
-def update_selected_markers(clickData, selected_markers):
-    if clickData is not None:
-        tooltip = clickData['points'][0]['customdata']
-        #tooltip = clickData['points'][0]['tooltip']
-        selected_markers.append(tooltip)
-        label = "Marcador seleccionado: " + tooltip
-        list_items = [html.Li(marker) for marker in selected_markers]
-        return selected_markers, label, list_items
-    else:
-        label = "Selecciona un marcador en el mapa"
-        return selected_markers, label, []
+
 if __name__ == '__main__':
     # Abrir el navegador con la aplicación Dash
     webbrowser.open('http://127.0.0.1:8050/')
