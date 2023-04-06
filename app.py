48fa345080849ef6ff2ca0663f5f892fdb6f7870
ahiruk <luisaguzmanidetp@gmail.com>
2023-04-06 12:51:11 -0500

    actualizacion 2 de marcadores

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ app = dash.Dash(__name__)
 
 capitales= pd.read_csv('europa.csv')
 locations = []
+nombre_capital=[]
 for i, row in capitales.iterrows():
     locations.append((row["Latitud"], row["Longitud"]))
+    nombre_capital.append((row["Capital"]))
 
 markers = [dl.CircleMarker(
     id=f"marker-{i}",
@@ -23,7 +25,7 @@ app.layout = html.Div([
     dl.Map(center=(52, 20), zoom=4.3, children=[
         dl.TileLayer(),
         *markers
-    ], style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}),
+    ], style={'width': '100%', 'height': '100vh', 'margin': "auto", "display": "block"}),
     html.Div(id="output")
 ])
 
@@ -38,7 +40,7 @@ def handle_click(*args):
         color = "red" if n_clicks is not None and n_clicks % 2 != 0 else "gray"
         colors.append(color)
         if color == "red":
-            selected_markers.append(f"Marker {i}")
+            selected_markers.append(f"{nombre_capital[i]}")
     return colors + [f"Selecteds: {', '.join(selected_markers)}"]
 
 if __name__ == '__main__':
